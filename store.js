b3ac376e7597927f8a32f7b7764d106b53522863
chandana-codes <84772049+chandana-codes@users.noreply.github.com>
2023-11-25 12:14:58 +0530

    create bussinessList

--- store.js
+++ store.js
@@ -1,6 +1,34 @@
+const businessListings = [];
+
 function create(businessListingRequest) {
+
+  const existingListing = businessListings.find(listing => listing.businessName === businessListingRequest.businessName);
+
+    if (existingListing) {
+        return null;
+    }
+    const businessListingId = generateUniqueId();
+    const newBusinessListing = {
+        businessListingId,
+        businessName: businessListingRequest.businessName,
+        ownerName: businessListingRequest.ownerName,
+        category: businessListingRequest.category,
+        city: businessListingRequest.city,
+        establishmentYear: businessListingRequest.establishmentYear
+    };
+
+    businessListings.push(newBusinessListing);
+    return newBusinessListing;
+  
+
+  function generateUniqueId() {
+    return Math.random().toString(36).substr(2, 9);
+  }
+
+
   return null;
-}
+
+};
 
 function read(id) {
   return null;
